0f4526f10fa7c081046a3d57321ec62c04bd9284
Alessandro Ingrosso <ingrossorama@gmail.com>
2020-05-07 00:34:03 -0400

    add options to print

--- src/interventions.c
+++ src/interventions.c
@@ -357,7 +357,7 @@ int intervention_quarantine_until(
 {
 	if( is_in_hospital( indiv ) )
 		return FALSE;
-	fprintf(stderr, "HERE\n");
+	// fprintf(stderr, "HERE\n");
 	if( indiv->traced_on_this_trace )
 		return;
 
@@ -395,6 +395,8 @@ int intervention_quarantine_until(
 	{
 		indiv->quarantine_event = add_individual_to_event_list( model, QUARANTINED, indiv, model->time );
 		set_quarantine_status( indiv, model->params, model->time, TRUE );
+		// ALE
+		//fprintf(stderr, "...quarantining the guy\n");
 	}
 
 	if( indiv->quarantine_release_event != NULL )
